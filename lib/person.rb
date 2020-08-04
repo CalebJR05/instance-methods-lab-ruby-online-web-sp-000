c290f1f955186e5b73b5add40491d41855535625
Caleb R <cjr4912@gmail.com>
2020-08-04 18:15:03 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,11 +1,12 @@
 class Person
   
-def talk
-  puts "Hello World!"
-  
-def walk
-  puts "The Person is walking"
-  
+  def talk
+    puts "Hello World!"
   end
+  
+  def walk
+    puts "The Person is walking"
+  end
+
 end
   
